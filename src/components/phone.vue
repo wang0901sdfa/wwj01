14ebda67e98d8ef7595705b5365c696d1acea0f5
unknown <3259416762@qq.com>
2024-05-28 16:42:44 +0800

    b

---
+++ src/components/phone.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="phone-page">
+      <van-nav-bar title="联系电话" left-arrow @click-left="handlephone" />
+    </div>
+    <div>
+        <van-button round block type="default" @click="handlephone">确定联系</van-button>
+        
+    </div>
+  </template>
+  
+  <script setup>
+  import { useRouter } from 'vue-router';
+  
+  const router = useRouter();
+  
+  function handlephone(){
+    router.push('/phoneNumber')
+  }
+  
+  </script>
+  
+  <style scoped>
+  .logout-page {
+    max-width: 400px;
+    margin: 0 auto;
+  }
+  
+  .logout-content {
+    padding: 16px;
+  }
+  
+  .logout-button-container {
+    display: flex;
+    justify-content: space-between;
+    margin-top: 20px;
+  }
+  </style>
+  
---
+++ src/components/phoneNumber.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1>联系电话:1234567890</h1>
+    </div>
+    <div>
+        <van-button round block type="danger" @click="confirm">返回上一页面</van-button>
+    </div>
+</template>
+
+<script>
+function confirm() {
+    goBack();
+  }
+function goBack() {
+    router.back();
+  }
+</script>
+
+<style scoped>
+
+</style>
